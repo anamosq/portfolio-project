96083a521f79b43783dc8e5a37dc2e2137e0de5e
anamosq <anamosquera@gmail.com>
2024-10-01 10:49:20 -0400

    Scripts

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+console.log ("Loading")
+alert (
+    "BREAKING NEWS!! Israel says it's raiding Hezbollah strongholds in Lebanon, group fires at Israel --- France says it is sending helicopter carrier to eastern Mediterranean"
+)
